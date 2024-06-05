ed084b368253eac66e6ea15ab7d99334ac8a59d8
Mwamtindi <mwamtindishaban@gmail.com>
2024-06-05 23:44:41 +0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,13 +10,17 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_ht, right_ht;
+	size_t left_ht = 0, right_ht = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_ht = binary_tree_height(tree->left);
-	right_ht = binary_tree_height(tree->right);
-
-	return ((left_ht > right_ht ? left_ht : right_ht) + 1);
+	if (tree->left)
+		left_ht = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_ht = binary_tree_height(tree->right) + 1;
+	if (left_ht >= right_ht)
+		return (left_ht);
+	else
+		return (right_ht);
 }
